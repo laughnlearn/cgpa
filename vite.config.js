1130993f9039649117133e830e65ba1bbc35eeb5
laughnlearn <laughnlearn1820@gmail.com>
2025-07-10 19:40:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cgpa/', // 👈 MUST match your GitHub repo name
+  base: '/cgpa/', // 👈 this MUST match your GitHub repo name exactly
   plugins: [react()],
-});
+})
